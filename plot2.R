a799d87d775e36cff47c10a3e003ea09402d8bdc
Matt Frei <mattfrei27@gmail.com>
2015-12-09 22:06:02 -0700

    First three plots

---
+++ plot2.R
@@ -0,0 +1,33 @@
+# File written by Matt Frei on 12/9/2015. 
+# Coursera Exploratory Data Analysis class
+# Project 1 - Plot 2
+
+setwd('C:/Users/matt/Dropbox/Documents/Coursera/exploratory data analysis/Project 1/ExData_Plotting1')
+
+######################################## Load Data ########################################
+
+sc <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(sc,'data.zip')
+unzip('data.zip')
+cols <- c('date','time','global_active_power','global_reactive_power','voltage','global_intensity','sub_metering_1','sub_metering_2','sub_metering_3')
+pd <- read.table('household_power_consumption.txt',
+                 header=FALSE,
+                 sep=';',
+                 na.strings='?',
+                 skip=66637,
+                 nrows=2880,
+                 col.names = cols,
+                 stringsAsFactors=FALSE)
+# I got the skip and nrows parameters using Bash commands:
+  # grep  -n '1/2/2007' household_power_consumption.txt | head -n 1
+  # grep  -n '^2/2/2007' household_power_consumption.txt | tail -n 1
+
+pd$dt <- strptime(paste(pd$date,' ',pd$time),format=' %d/%m/%Y%H:%M:%S')
+
+######################################## Begin plot 2 ########################################
+
+png('plot2.png',width=480,height=480)
+plot(pd$dt,pd$global_active_power,
+     ylab = 'Global Active Power (kilowatts)',
+     xlab='', type='l')
+dev.off() ## Don't forget to close the PNG device!
